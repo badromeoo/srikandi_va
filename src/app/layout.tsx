b457b7f9f559f810d00bc640627cc1b7ac9fd087
I Made Bintang Astawa <astawabintang@gmail.com>
2025-09-04 10:33:10 +0800

    form applicant, toast notif, supabase edge function submit-applicant, add tabs shadcn on request page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar"; 
-
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "Srikandi VA",
@@ -22,6 +22,7 @@ export default function RootLayout({
       <head>
       </head>
       <body>
+        <Toaster position="top-right" />
         <Navbar />
         {children}
       </body>
